e3f3859d8d566c01b9b49168a403f75574e8ccee
Mike Oliver <remoteunderscore@gmail.com>
2022-11-24 15:49:28 +0000

    First Rspec test passed on UserAccountRepository

--- lib/user_account_repository.rb
+++ lib/user_account_repository.rb
@@ -1,3 +1,23 @@
 class UserAccountRepository
+  def all
+    sql = 'SELECT id, email_address, username FROM user_accounts;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    user_accounts = []
+
+    result_set.each do |record|
+      user_account = UserAccount.new
+      user_account.id = record['id']
+      user_account.email_address = record['email_address']
+      user_account_username = record['username']
+
+      user_accounts << user_account
+    end
+      return user_accounts
+    end
+
+
+
+
 
 end
--- spec/user_account_repository_spec.rb
+++ spec/user_account_repository_spec.rb
@@ -1,23 +1,29 @@
 require 'user_account_repository'
+require 'user_account'
 
 RSpec.describe UserAccountRepository do
 
-def reset_user_accounts_table
-  seed_sql = File.read('spec/seeds_user_accounts.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
-  connection.exec(seed_sql)
-end
-
-describe UserAccountRepository do
-  before(:each) do 
-    reset_user_accounts_table
+  def reset_user_accounts_table
+    seed_sql = File.read('spec/seeds_user_accounts.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
+    connection.exec(seed_sql)
   end
 
+    before(:each) do 
+      reset_user_accounts_table
+    end
+
+    it "returns a list of user accounts" do
+      repo = UserAccountRepository.new
+
+      user_accounts = repo.all
+      expect(user_accounts.length).to eq (4)
+      expect(user_accounts.first.id).to eq ('1')
+      expect(user_accounts.first.email_address).to eq ('mike@mike')
+    end
 
 
 
 
 
-
-end
 end
